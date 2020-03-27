a1df38e8e1d91f67c927cb59ab962bf839ca211a
wangmengmeng <wmm_0165@163.com>
2020-03-27 22:41:53 +0800

    数据写入json文件时保证样式美观且中文正确显示

--- utils/handle_json.py
+++ utils/handle_json.py
@@ -9,7 +9,7 @@ def write_data(data, filename):
     file_path = os.path.join(config_path, filename)
     # print(file_path)
     with open(file_path, 'w', encoding='utf8') as f:  # 如果json文件不存在会自动创建
-        json.dump(data, f)
+        json.dump(data, f,indent=4,ensure_ascii=False)
         print("数据写入json文件完成...")
 
 
